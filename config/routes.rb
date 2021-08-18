f2cb5f8d751e88137c48814cff076b2d50b6e4bf
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-08-18 18:06:51 -0400

    creacion ruta destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources: list, only: [:index, :show, :new, :create] do
-    resources: bookmarks, only: [:new, :create, :destroy]
+  resources: list, only: [ :index, :show, :new, :create ] do
+    resources: bookmarks, only: [ :new, :create ]
   end
+
+  resources: list, only: [ :destroy ]
+  
 end
