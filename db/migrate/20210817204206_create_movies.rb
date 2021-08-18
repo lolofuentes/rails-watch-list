7a61793e9bd4a62dd51778cb237132f0ac506d55
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-08-17 22:51:13 -0400

    la verdad es que no sé que tengo mal, al parecer el migrate del modelo movie me quedó mal porque el migrate me escribió tittle con 2 t y over_view asi, y yo en los modelos usaba title y overview, modifiqué todo y me aceptó otras cosas de list en verde pero me rindo, todos los errores hacen referencia a title o algo vinculado a ello

--- db/migrate/20210817204206_create_movies.rb
+++ db/migrate/20210817204206_create_movies.rb
@@ -1,8 +1,8 @@
 class CreateMovies < ActiveRecord::Migration[6.0]
   def change
     create_table :movies do |t|
-      t.string :tittle
-      t.text :over_view
+      t.string :title
+      t.text :overview
       t.text :poster_url
       t.float :rating
 
