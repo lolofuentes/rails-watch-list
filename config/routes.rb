01b8df7959a787f8956508aaad9056286eb9d417
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-08-18 18:02:43 -0400

    creacion de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources: list, only: [:index, :show, :new, :create] do
+    resources: bookmarks, only: [:new, :create, :destroy]
+  end
 end
