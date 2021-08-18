c799288413e7f340f3edcd3a34cc5800ea8eafbd
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-08-18 17:38:27 -0400

    apenas y cree los metodos del list

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,2 +1,27 @@
 class ListController < ApplicationController
+
+    def index
+        @list = List.all    
+    end
+
+    def new
+        @list = List.new
+    end
+
+    def create
+        @list = List.new (list_params)
+        @list.save
+    end
+
+    def show
+        @list = List.find (params[:id])
+        @list.bookmarks
+    end
+
+    private
+
+    def list_params
+        params.require(:list)permit(:name)
+    end
+
 end
