c98549078d6e4f0881b1f5d73cfef9711bb4938c
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-08-23 11:33:35 -0400

    creacion formulario en show y modificaciones al orden, modificación a ultima linea a seed, modificación a rutas para que pag principal abra en local host3000 en vez de /lists

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -26,7 +26,7 @@ class BookmarksController < ApplicationController
     end
 
     def bookmark_params
-        params.require(:bookmark)permit(:comment, :movie_id, :list_id)
+        params.require(:bookmark).permit(:comment, :movie_id, :list_id)
     end
     
 end
